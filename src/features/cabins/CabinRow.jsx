10b6982531b9620658b514838ffa3175e5bb4292
ortursucceeh <109922331+ortursucceeh@users.noreply.github.com>
2023-07-04 16:41:31 +0300

    add delete cabin func and connect react-toasts

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,5 +1,8 @@
 import styled from "styled-components";
 import { formatCurrency } from "../../utils/helpers";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { deleteCabin } from "../../services/apiCabins";
+import { toast } from "react-hot-toast";
 
 const TableRow = styled.div`
   display: grid;
@@ -41,7 +44,28 @@ const Discount = styled.div`
 `;
 
 function CabinRow({ cabin }) {
-  const { name, maxCapacity, regularPrice, discount, image } = cabin;
+  const {
+    id: cabinId,
+    name,
+    maxCapacity,
+    regularPrice,
+    discount,
+    image,
+  } = cabin;
+
+  const queryClient = useQueryClient();
+
+  const { isLoading: isDeleting, mutate } = useMutation({
+    mutationFn: deleteCabin, //(id) => deleteCabin(id),
+    onSuccess: () => {
+      toast.success("Cabin was successfully deleted");
+      queryClient.invalidateQueries({
+        queryKey: ["cabins"],
+      });
+    },
+    onError: (err) => toast.err(err.message),
+  });
+
   return (
     <TableRow role="row">
       <Img src={image} alt="" />
@@ -49,7 +73,9 @@ function CabinRow({ cabin }) {
       <div>Fits up to {maxCapacity}</div>
       <Price>{formatCurrency(regularPrice)}</Price>
       <Discount>{formatCurrency(discount)}</Discount>
-      <button>Delete</button>
+      <button onClick={() => mutate(cabinId)} disabled={isDeleting}>
+        Delete
+      </button>
     </TableRow>
   );
 }
--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -5,8 +5,16 @@ export async function getCabins() {
 
   if (error) {
     console.log(error);
-    throw new Error("cabins could not be loaded!");
+    throw new Error("Cabins could not be loaded!");
   }
 
   return data;
+}
+
+export async function deleteCabin(id) {
+  const { error } = await supabase.from("cabins").delete().eq("id", id);
+  if (error) {
+    console.log(error);
+    throw new Error("Cabin could not be deleted!");
+  }
 }
