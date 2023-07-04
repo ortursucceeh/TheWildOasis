b228a208ce1636e091b40e2bcb3506e9d4aa5133
ortursucceeh <109922331+ortursucceeh@users.noreply.github.com>
2023-07-04 17:04:59 +0300

    add create cabin by using react-hook-form

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -11,6 +11,18 @@ export async function getCabins() {
   return data;
 }
 
+export async function createCabin(newCabin) {
+  const { data, error } = await supabase
+    .from("cabins")
+    .insert([newCabin]) // because form inputs have the same names as a newCabin's object keys
+    .select();
+
+  if (error) {
+    console.log(error);
+    throw new Error("Cabin could not be created!");
+  }
+}
+
 export async function deleteCabin(id) {
   const { error } = await supabase.from("cabins").delete().eq("id", id);
   if (error) {
